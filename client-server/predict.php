c5407d17983fdbbe280ebb2fec4c75b6fcd768c9
Hakam Chedo <ha-kam009@hotmail.com>
2024-01-05 20:50:09 +0300

    -Predict sucsess

--- client-server/predict.php
+++ client-server/predict.php
@@ -49,40 +49,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         async function prediction() {
             const model = await tf.loadLayersModel("../Model/model.json");
 
-            
-            
-            // Create an array of passenger data
-            // Create an array of passenger data for example 
-            let passenger = [[1], [1], [38], [1], [0], [71.2833], [0]];  // Assuming Pclass=1 for simplicity
-
-            // Convert the array to a tensor and reshape it to [7, 1]
-            let input = tf.tensor(passenger);
-            // Reshape the tensor to 3D
-            input = input.reshape([1, 7, 1]);
-            
             //input = input.reshape([1, 7,1]);
-            // const inputDataString = "<?php echo $input_data; ?>";
-            // const inputDataArray = JSON.parse(inputDataString).map(Number);
-            //const input = tf.tensor3d([inputDataArray], [1, 1, 7]);
-            //console.log(input);
+            const inputDataString = "<?php echo $input_data; ?>";
+            const inputDataArray = JSON.parse(inputDataString).map(Number);
+            const input = tf.tensor3d(inputDataArray, [1, 7, 1]);
+            console.log(input);
 
              // Load the scaler information
             const scalerInfoResponse = await fetch("../Model/scaler_info.json");
             const scalerInfo = await scalerInfoResponse.json();
             // Create a scaler using the information
             const dataScaler = {
-                mean: tf.tensor(scalerInfo.mean),
-                scale: tf.tensor(scalerInfo.scale),
+                mean: tf.tensor(scalerInfo.mean).reshape([1, 7, 1]),
+                scale: tf.tensor(scalerInfo.scale).reshape([1, 7, 1]),
             };
             //scale the passenger data using the same scaler used during training
             const scaledInput = input.sub(dataScaler.mean).div(dataScaler.scale);
-            // Scale the passenger data
+            // reshape scaled input to 3D with shape [1, 7, 1]
+            console.log(scaledInput);
             // Make a prediction
             const output = model.predict(scaledInput);
             const prediction = Array.from(output.dataSync())[0];
 
             // Display the prediction in a popup
-            alert("Survival Probability: " + prediction);
+            alert("Survival Rate: " + prediction*100+"%");
         }
     </script>
 </body>
