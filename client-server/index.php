280e8f8cd50f732c5053846ee33f82e8a529c46b
Hakam Chedo <ha-kam009@hotmail.com>
2024-01-08 20:29:15 +0300

    -Last Update

--- client-server/index.php
+++ client-server/index.php
@@ -9,6 +9,7 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+        <!-- Post data -->
         <script>
         function postData() {
             var formData = $("#form1").serialize(); // Serialize form data
@@ -19,11 +20,9 @@
                 data: formData,
                 success: async function(response) {
                     const predicted = await prediction(response); // Call prediction function from Prediction.js
-                    // alert(predicted);
-                    // Display prediction result to predictionResult div as "Survived" or "Not Survived if predicted value is more than 0.5"
-                    // $("#predictionResult").html(predicted > 0.5 ? "<h1 class='text-success'>This Passenger was Survived from Titanic incident</h1>" 
-                    // : "<h1 class='text-danger'>This Passenger was not Survived from Titanic incident</h1>");
+                    // Set threshold to 0.453
                     $threshold = 0.453
+                    // Display result
                     $("#predictionResult").html(predicted > $threshold ? '<div class="alert alert-dismissible alert-success" style="width:100%;"><button type="button" class="btn-close" data-bs-dismiss="alert"></button><h3><strong>This Passenger</strong> was <strong>Survived</strong> from Titanic incident.</h3></div>' 
                     : '<div class="alert alert-dismissible alert-danger" style="width:100%;"><button type="button" class="btn-close" data-bs-dismiss="alert"></button><h3><strong>This Passenger</strong> was <strong>not Survived</strong> from Titanic incident.</h3></div>');
                     
@@ -31,10 +30,12 @@
             });
         }
         </script>
+        <!-- Background video -->
         <audio id="song" controls>
             <source src="../images&videos/titanic_song.mp3" type="audio/mp3">
             Tarayıcınız ses etiketini desteklemiyor.
         </audio>
+        <!-- toggleSound -->
         <script>
             var audioElement = document.getElementById("song");
 
@@ -52,6 +53,7 @@
                 }
             }
         </script>
+        <!-- TensorFlow.js -->
         <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs"></script>
         <!-- Include Prediction.js -->
         <script src="Prediction.js"></script>
@@ -66,37 +68,48 @@
                     <video id="videoBackground" autoplay muted loop>
                         <source src="../images&videos/titanic.mp4" type="video/mp4">
                     </video>
+                    <!-- Sound mute unmute button -->
                     <div class="col-md-2 ">
                         <br />
                         <div class="row">
                             <button type="button"onclick="toggleSound()" id="song_button"><i class="bi bi-volume-mute-fill text-primary" style=" font-size: 24px;"></i></button>
                         </div>
                     </div>
+                    <!-- End Sound mute unmute button -->
+                    <!-- Header Part -->
                     <div class="col-md-8 ">
                         <br />
+                        <!-- Title -->
                         <div class="row">
                             <h1 class="d-flex justify-content-center text-white">Titanic Survival Prediction</h1>
                         </div>
+                        <!-- End Title -->
+                        <!-- Result -->
                         <div class="row d-flex justify-content-center " style="height: 90vh;">
                             <div class="my-auto d-flex " id="predictionResult" >
-
+                                <!-- Result Will Show here -->
                             </div>
                         </div>
-                        
+                        <!-- End Result -->
                     </div>
+                    <!-- End Header Part -->
                     <div class="col-md-2 "></div>
-                    
                 </div>
                 <!-- End Left background -->
+                <!-- User Form Part  -->
                 <div class="col d-flex justify-content-center bg-light" style="height: 100vh;">  
                     <div class="my-auto " >
+                        <!-- Header -->
                         <div class="row">
                             <h2><label class="form-label mt-4 text-primary">Titanic Passenger details</label></h2>
                         </div>
+                        <!-- End Header -->
                         <!-- form group -->
                         <div class="row">
                             <div class="col-md-10 mx-auto">
+                                <!-- 1st row -->
                                 <div class="row">
+                                    <!-- Ticket Class -->
                                     <div class="col">
                                         <div class=" form-group">
                                             <label class="form-label mt-4">Ticket Class</label>
@@ -108,6 +121,8 @@
                                             </select>
                                         </div>
                                     </div>
+                                    <!-- End Ticket Class -->
+                                    <!-- Gender -->
                                     <div class="col">
                                         <div class=" form-group">
                                             <label class="form-label mt-4">Gender</label>
@@ -116,30 +131,38 @@
                                                 <option value="male">Male</option>
                                                 <option value="female">Female</option>
                                             </select>
-                                            <!-- <input type="text" class="form-control" name="pclass" required> -->
                                         </div>
                                     </div>
+                                    <!-- End Gender -->
+                                    <!-- Age -->
                                     <div class="col">
                                         <div class=" form-group">
                                             <label class="form-label mt-4">Age</label>
                                             <input type="number" class="form-control form-text" name="age" required>
                                         </div>
                                     </div>
+                                    <!-- End Age -->
                                 </div>
+                                <!-- End 1st row -->
+                                <!-- 2nd row -->
                                 <div class="row">
+                                    <!-- sibsp -->
                                     <div class="col">
                                         <div class=" form-group">
                                             <label class="form-label mt-4">Number of Sibling/Spouse</label>
                                             <input type="number" class="form-control form-text" name="sibsp" required>
                                         </div>
                                     </div>
+                                    <!-- End sibsp -->
+                                    <!-- parch -->
                                     <div class="col">
                                         <div class=" form-group">    
                                             <label class="form-label mt-4">Number of Parents/Child</label>
                                             <input type="number" class="form-control form-text" name="parch" required>
                                         </div>
                                     </div>
-                                    
+                                    <!-- End parch -->
+                                    <!-- fare -->
                                     <div class="col">
                                     <br />
                                         <div class=" form-group">
@@ -147,8 +170,12 @@
                                             <input type="number" class="form-control form-text" name="fare" required>
                                         </div>
                                     </div>
+                                    <!-- End fare -->
                                 </div>
+                                <!-- End 2nd row -->
+                                <!-- 3rd row -->
                                 <div class="row">
+                                    <!-- embarked -->
                                     <div class="col">
                                         <div class=" form-group">
                                             <label class="form-label mt-4">Port of Embarkation</label>
@@ -160,16 +187,21 @@
                                             </select>
                                         </div>
                                     </div>
+                                    <!-- End embarked -->
                                 </div>
+                                <!-- End 3rd row -->
                                 <br />
+                                <!-- Predict button -->
                                 <div class=" d-grid gap-2">
                                     <button type="button" onclick="postData()" id="predict" name="predict" class="btn btn-primary"">Predict</button>
                                 </div>  
-                                
+                                <!-- End Predict button -->
                             </div>
                         </div> 
+                        <!-- End form group -->
                     </div>
                 </div>
+                <!-- End User Form Part  -->
             </div>
         </form>
     </body>
